8aa06266b7480b380cf693e55f6c5e3d2eaa11d3
Yawn0 <77552501+Yawn0@users.noreply.github.com>
2022-10-01 14:26:02 +0200

    Add project files.

---
+++ ConsoleApp3/Exercises/BinaryGap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgrammingExs.Exercises
+{
+    public class BinaryGap
+    {
+        public static int Solution(int N)
+        {
+            if (N >= int.MaxValue || N <= 4)
+            {
+                return 0;
+            }
+
+            char[] bitArray = Convert.ToString(N, 2).ToCharArray();
+
+            List<int> binaryGaps = new List<int>() { 0 };
+            int binaryGapCount = 0;
+
+            bool end;
+
+            for (int i = 0; i < bitArray.Length; i++)
+            {
+                end = bitArray[i] == '1';
+
+                if (end && binaryGapCount > 0)
+                {
+                    binaryGaps.Add(binaryGapCount);
+                    binaryGapCount = 0;
+                }
+                else if (!end)
+                {
+                    binaryGapCount++;
+                }
+            }
+
+            return binaryGaps.Max();
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿using CompetitiveProgrammingExs.Exercises;
+using System.Collections;
+using System.Numerics;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int result = BinaryGap.Solution(1610612737);
+
+        Console.WriteLine(result);
+    }
+}
